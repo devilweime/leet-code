9e84721d307c5437b0b69633f2bb22e36f3bd883
devilweime <1665382553@>
2020-05-26 22:38:40 +0800

    重新整理

--- src/main/java/com/example/t739/Solution1.java
+++ src/main/java/com/example/t739/Solution1.java
@@ -12,16 +12,16 @@ public class Solution1 {
 
     public int[] dailyTemperatures(int[] T) {
         int len = T.length;
-        int[] days  = new int[len];
+        int[] days = new int[len];
 
         for (int i = 0; i < len; i++) {
             int before = T[i];
             int count = 0;
-            for (int j = i+1; j< len; j++){
-                count ++;
-                if (T[j] > before){
+            for (int j = i + 1; j < len; j++) {
+                count++;
+                if (T[j] > before) {
                     break;
-                }else if (j == len-1){//最后一个比完还是没有比压栈的大
+                } else if (j == len - 1) {//最后一个比完还是没有比压栈的大
                     count = 0;
                 }
             }
